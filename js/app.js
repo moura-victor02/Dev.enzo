43cdf2931e438d77f19164dfca26cdd378f7010f
Enzo Victor <enzomoru3@gmail.com>
2023-03-17 14:04:47 -0300

    att

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
 const header = document.querySelector('.header.container');
 
+
 hamburger.addEventListener('click', () => {
 	hamburger.classList.toggle('active');
 	mobile_menu.classList.toggle('active');
@@ -25,6 +26,11 @@ menu_item.forEach((item) => {
 });
 
 var urlprojeto1 = document.querySelector(".project-img");
+urlprojeto1.addEventListener("click", function() {
+    window.open("https://moura-victor02.github.io/nav-bar/", "_blank");
+});
+
+var urlprojeto1 = document.querySelector(".contact-item");
 urlprojeto1.addEventListener("click", function() {
     window.open("https://moura-victor02.github.io/nav-bar/", "_blank");
 });
